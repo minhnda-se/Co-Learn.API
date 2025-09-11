7d44c03e26b8557d588555d1eb8c6d2ba21ba6fb
minhnda-se <anhminh02111@gmail.com>
2025-09-11 13:00:06 +0700

    Update UnitOfWork

--- CoLearn.Infrastructure/UnitOfWork.cs
+++ CoLearn.Infrastructure/UnitOfWork.cs
@@ -1,13 +1,35 @@
 ﻿using CoLearn.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using CoLearn.Domain.Interfaces.Repositories;
+using CoLearn.Infrastructure.Context;
+using CoLearn.Infrastructure.Models;
+using CoLearn.Infrastructure.Repositories;
 using System.Threading.Tasks;
 
 namespace CoLearn.Infrastructure
 {
     public class UnitOfWork : IUnitOfWork
     {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+
+            // Khởi tạo repository
+            UserProfileCustom = new UserProfileRepository(_context);
+        }
+        public IUserProfileRepository UserProfileCustom { get; private set; }
+
+        // Commit tất cả thay đổi
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        // Dispose DbContext
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
