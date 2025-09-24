e3ed44af14032585311ecccbc7502486ed64ab45
minhnda-se <anhminh02111@gmail.com>
2025-09-24 11:40:43 +0700

    Fix CourseRepo
    Delete GetAllCourse no param

--- CoLearn.Domain/Interfaces/Repositories/ICourseRepository.cs
+++ CoLearn.Domain/Interfaces/Repositories/ICourseRepository.cs
@@ -10,8 +10,6 @@ namespace CoLearn.Domain.Interfaces.Repositories
 {
     public interface ICourseRepository : IGenericRepository<Course>
     {
-
-        Task<List<Course>> GetAllCourseAsync();
         Task<List<Course>> GetAllCourseByTeacherId(int teacherId);
 
         Task<PagedResult<Course>> GetAllCourseAsync(int pageIndex, int pageSize);
