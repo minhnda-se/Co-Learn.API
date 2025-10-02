aa85a7ad4a212142c4832a77703ee1088f91128f
minhnda-se <anhminh02111@gmail.com>
2025-10-03 00:06:06 +0700

    Fix conflict

--- CoLearn.Domain/Interfaces/IUnitOfWork.cs
+++ CoLearn.Domain/Interfaces/IUnitOfWork.cs
@@ -19,14 +19,13 @@ namespace CoLearn.Domain.Interfaces
         IAssignmentRepository AssignmentRepository { get; }
         ICourseMaterialRepository CourseMaterialRepository { get; }
         IEnrollmentRepository EnrollmentRepository { get; }
-<<<<<<< HEAD
+
         ISubmissionRepository SubmissionRepository { get; }
         IScheduleRepository ScheduleRepository { get; }
 
-=======
+
         IBookingRepository BookingRepository { get; }
->>>>>>> 4d1b3b33982aedec6c347bdf4ae48c36c8096abe
-        // Generic Repositories
+
         IGenericRepository<User> UserGenericRepository { get; }
 
         //  Repositories 
